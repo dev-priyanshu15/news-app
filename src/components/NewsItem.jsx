28356dc7746fc8ccadd3b3081134ca49c5ecc2bb
Priyanshu Singh <123263608+spriyanshucoder@users.noreply.github.com>
2024-06-21 23:44:32 +0530

    Update NewsItem.jsx

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -1,7 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import { twMerge } from "tailwind-merge";
 import { Link } from "react-router-dom";
-
 import { addBookmark, removeBookmark } from "../features/news/newsSlice";
 import Image from "./Image";
 import NewsItemHeader from "./NewsItemHeader";
@@ -24,30 +23,23 @@ const NewsItem = ({ article, index }) => {
     }
   };
 
-  const flexDirection = index % 2 === 0 ? "md:flex-row-reverse" : "md:flex-row";
+  const flexDirection = index % 2 === 0 ? "mt-8 md:mt-16" : "mt-16 md:mt-12";
 
   return (
-    <div
-      className={twMerge(
-        "p-4 border bg-[#edf5fe] flex flex-col gap-4 overflow-hidden rounded-lg shadow-sm",
-        flexDirection
-      )}
-    >
-      <Link
-        to={`news/${encodedUrl}`}
-        className="h-52 md:h-40 w-full md:w-1/4 rounded-md overflow-hidden"
-      >
+    <div className="mt-16 border-spacing-1 p-4 border bg-orange-100 flex flex-col gap-4 overflow-hidden rounded-lg shadow-sm">
+      <Link to={`news/${encodedUrl}`} className="relative block w-full md:w-1/4 h-52 md:h-40 rounded-md overflow-hidden">
         <Image
           src={article.thumbnail}
           alt={article.title}
           title={article.title}
+          className="object-cover w-full h-full"
         />
       </Link>
-      <div className="flex flex-col flex-1">
+      <div className="flex flex-col flex-1 md:pl-4">
         <NewsItemHeader article={article} />
         <Link to={`news/${encodedUrl}`} className="flex-1 py-2">
-          <h2 className="text-lg font-bold md:line-clamp-1">{article.title}</h2>
-          <p className="line-clamp-2">{article.excerpt}</p>
+          <h2 className="text-lg font-bold md:line-clamp-2">{article.title}</h2>
+          <p className="line-clamp-3">{article.excerpt}</p>
         </Link>
         <NewsItemFooter
           article={article}
